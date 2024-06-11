edae19016bd2efc127686402e83ea08570de511f
thanhcathay-ins <duonghuuthanh1306@gmail.com>
2024-06-11 21:07:54 +0700

    Initial commit

--- src/main/java/com/example/demo/HomeController.java
+++ src/main/java/com/example/demo/HomeController.java
@@ -18,7 +18,7 @@ public class HomeController {
     @GetMapping("/{name}")
     public String hello(@PathVariable String name){
         log.info("Request with name {}", name);
-        return "Heyyy" + name;
+        return "Test CI/CD" + name;
     }
 
 }
